b287007b4d1828542cb38f5637182ec30015fcf0
ychenolivia <45774566+ychenolivia@users.noreply.github.com>
2020-01-10 09:27:25 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Full stack dev!';
+  correctFood = new Set(["onion egg ", "tomato egg ", "chili porkbelly ", "potato porkbelly ", "egg onion ", "egg tomato ", "porkbelly chili ", "porkbelly chili "]);
+  food = "";
+  True=false;
+  False=false;
+
+  addFood1() {
+    this.food = this.food.concat("egg ");
+  }
+
+  addFood2() {
+    this.food = this.food.concat("onion ");
+  }
+
+  addFood3() {
+    this.food = this.food.concat("potato ");
+  }
+
+  addFood4() {
+    this.food = this.food.concat("tomato ");
+  }
+
+  addFood5() {
+    this.food = this.food.concat("chili ");
+  }
+
+  addFood6() {
+    this.food = this.food.concat("porkbelly ");
+  }
+
+  check() {
+    if(this.correctFood.has(this.food)){
+      this.True = true;
+    }
+    else{
+      this.False = true;
+    }
+    this.food = ""
+  }
 }
